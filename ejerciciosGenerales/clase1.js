5d9d75edf383bc41eeead982d83d70d768d2316d
andrea.pennisi <andrea.pennisi@connect.auto>
2023-04-16 12:16:59 +0100

    "improving root"

---
+++ ejerciciosGenerales/clase1.js
@@ -0,0 +1,33 @@
+// clase1
+let edadActual = parseInt(propmt("Ingrese su edad"));
+let edadEn5 = edadActual + 5;
+console.log(edadEn5);
+alert("En 5 años tendras " + edadEn5 + "años");
+
+// ClaseFunciones
+function wrapping(gifts) {
+  const wrappedGifts = [];
+
+  for (const gift of gifts) {
+    const length = gift.length;
+    const totalChars = length + 4;
+    const wrappedGift = new Array(totalChars).fill("*");
+
+    wrappedGift[0] = wrappedGift[totalChars - 1] = "*";
+    wrappedGift[1] = wrappedGift[totalChars - 2] = " ";
+
+    const mid = Math.floor(totalChars / 2);
+    for (let i = 0; i < length; i++) {
+      wrappedGift[mid - Math.floor(length / 2) + i] = gift[i];
+    }
+
+    wrappedGifts.push(wrappedGift.join("") + "\n");
+  }
+
+  return wrappedGifts;
+}
+
+const gifts = ["cat", "game", "socks"];
+const wrapped = wrapping(gifts);
+
+console.log(wrapped);
